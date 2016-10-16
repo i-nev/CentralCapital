50cfc3ac6dde7a4b5b5a62f9f959f694dd5b4a01
NevedrovAS <nevedrov@mineframe.ru>
2016-10-16 17:22:50 +0300

    Добавлена функция расчета расстояния

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	//"io"
 	"io/ioutil"
+	"math"
 	"net/http"
 	//"strings"
 	"net/url"
@@ -34,7 +35,7 @@ func GetURL(rUrl *url.URL) ([]byte, error) {
 	return body, nil
 }
 
-func main() {
+func GetCapitals(cs *[]Country) {
 	var (
 		mUrl             url.URL
 		query            url.Values
@@ -44,8 +45,8 @@ func main() {
 		geonamesCounties map[string][]map[string]interface{}
 		geonamesCapital  map[string]interface{}
 		capitals         []interface{}
-		countries        []Country
 		cap              map[string]interface{}
+		c                Country
 	)
 	mUrl.Scheme = "http"
 	mUrl.Host = "api.geonames.org"
@@ -74,7 +75,6 @@ func main() {
 	caCount := 0
 	coordCount := 0
 	for i, _ := range geonamesCounties["geonames"] {
-		var c Country
 		c.capital = geonamesCounties["geonames"][i]["capital"].(string)
 		c.countryCode = geonamesCounties["geonames"][i]["countryCode"].(string)
 		c.countryName = geonamesCounties["geonames"][i]["countryName"].(string)
@@ -104,6 +104,7 @@ func main() {
 			}
 			if findCoord {
 				coordCount++
+				*cs = append(*cs, c)
 			} else {
 				fmt.Printf("Oops: no PPLC for capital %s!\n", c.capital)
 			}
@@ -111,14 +112,72 @@ func main() {
 		} else {
 			fmt.Printf("Oops: country %s has no capital!\n", c.countryName)
 		}
-
-		countries = append(countries, c)
 		coCount++
 	}
 	fmt.Printf("Total: %d countries, %d capitals, %d coordinates\n", coCount, caCount, coordCount)
 
-	//for i, c := range countries {
-	//	fmt.Printf("%v -> %v\n", i, c)
-	//}
+	jsonCapital, err = json.Marshal(*cs)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", string(jsonCapital))
+	err = ioutil.WriteFile("capitals.json", jsonCapital, 0777)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func LoadCapitals(cs *[]Country, fromWeb bool) {
+	if fromWeb {
+		GetCapitals(cs)
+	}
+}
+
+func dist(a Country, b Country) float64 {
+	const rad = 6372795
+
+	aLat := a.lat * math.Pi / 180
+	bLat := b.lat * math.Pi / 180
+	aLng := a.lng * math.Pi / 180
+	bLng := b.lng * math.Pi / 180
+
+	cl1 := math.Cos(aLat)
+	cl2 := math.Cos(bLat)
+	sl1 := math.Sin(aLat)
+	sl2 := math.Sin(bLat)
+	delta := bLng - aLng
+	cdelta := math.Cos(delta)
+	sdelta := math.Sin(delta)
+
+	y := math.Sqrt(math.Pow(cl2*sdelta, 2) + math.Pow(cl1*sl2-sl1*cl2*cdelta, 2))
+	x := sl1*sl2 + cl1*cl2*cdelta
+	ad := math.Atan2(y, x)
+	return ad * rad
+}
+
+func distToCC(cc Country, cs *[]Country) float64 {
+	var total float64 = 0
+	for _, c := range *cs {
+		total += dist(cc, c)
+	}
+	return total
+}
+
+func main() {
+	var (
+		countries []Country
+		lat       float64
+		lng       float64
+	)
+	LoadCapitals(&countries, true)
+
+	for lat = -90; lat <= 90; lat += 20 {
+		for lng = -180; lng <= 180; lng += 20 {
+			var cc Country
+			cc.lat = lat
+			cc.lng = lng
+			fmt.Printf("dist= %v\n", distToCC(cc, &countries))
+		}
+	}
 
 }
